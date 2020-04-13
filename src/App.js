c83a931a4c0c42e4d660011ca60681d78d024b84
Ian <ian.shane.sears@gmail.com>
2020-04-12 17:37:54 -0700

    initial setup can get vaLue from input and display in alert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chat from './components/chat';
+
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Chat />
     </div>
   );
 }
